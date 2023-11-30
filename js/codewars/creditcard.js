402e7aa31033715fc78b28e45d3bac03e2228e6e
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-11-30 13:55:10 -0500

    maskify

--- js/codewars/creditcard.js
+++ js/codewars/creditcard.js
@@ -2,16 +2,16 @@
 function maskify(cc) {
   let result = "";
 
+  //Loop through string adding #'s to result string
   for (let i = 0; i < cc.length - 4; i++) {
     result += "#";
   }
+  //Add remaining 4 characters.
   for (let i = cc.length - 4; i < cc.length; i++) {
     result += cc[i];
   }
 
   return result;
 }
-
-
 
 console.log(maskify("4556364607935616"));
