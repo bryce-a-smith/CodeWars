f8ef06bf0d336cbf1eaf9bc7f34f7add6e3e8183
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-11-30 13:58:13 -0500

    optimize

--- js/codewars/creditcard.js
+++ js/codewars/creditcard.js
@@ -1,17 +1,22 @@
-// return masked string
+// // return masked string
+// function maskify(cc) {
+//   let result = "";
+
+//   //Loop through string adding #'s to result string
+//   for (let i = 0; i < cc.length - 4; i++) {
+//     result += "#";
+//   }
+//   //Add remaining 4 characters.
+//   for (let i = cc.length - 4; i < cc.length; i++) {
+//     result += cc[i];
+//   }
+
+//   return result;
+// }
+
 function maskify(cc) {
-  let result = "";
-
-  //Loop through string adding #'s to result string
-  for (let i = 0; i < cc.length - 4; i++) {
-    result += "#";
-  }
-  //Add remaining 4 characters.
-  for (let i = cc.length - 4; i < cc.length; i++) {
-    result += cc[i];
-  }
-
-  return result;
+    //Replace frst part of string with #'s. Add last 4 of string to end.
+    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
 }
 
 console.log(maskify("4556364607935616"));
