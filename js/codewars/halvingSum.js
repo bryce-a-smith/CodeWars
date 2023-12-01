dcf44092a4d28a37ce3c195083603be03d9585b2
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-12-01 13:09:34 -0500

    improved

--- js/codewars/halvingSum.js
+++ js/codewars/halvingSum.js
@@ -1,10 +1,18 @@
-function halvingSum(n) {
-  //Start with value of n.
-  let sum = n;
-  //Loop to sum the continuously halving numbers.
-  for (let i = 2; i <= n; i *= 2) {
-    sum += Math.floor(n / i);
-  }
+// function halvingSum(n) {
+//   //Start with value of n.
+//   let sum = n;
+//   //Loop to sum the continuously halving numbers.
+//   for (let i = 2; i <= n; i *= 2) {
+//     sum += Math.floor(n / i);
+//   }
 
-  return sum;
+//   return sum;
+// }
+
+function halvingSum(n) {
+  if (n === 1) {
+    return n;
+  } else {
+    return n + halvingSum(Math.floor(n / 2));
+  }
 }
--- js/codewars/powerOfTwo.js
+++ js/codewars/powerOfTwo.js
@@ -11,19 +11,26 @@
 //   }
 // }
 
+/////////////////////////////////////
+
+// function isPowerOfTwo(n) {
+//   //1 is a power of everything
+//   if (n === 1) {
+//     return true;
+//   } else {
+//     //Loop to continually divide n by 2.
+//     while (n > 1) {
+//       if (n % 2 != 0) {
+//         return false;
+//       }
+//       n = n / 2;
+//     }
+//   }
+//   //If an even number is divided by 2 to eventually equal 1, it is a power of 2.
+//   return n === 1;
+// }
+
 function isPowerOfTwo(n) {
-  //1 is a power of everything
-  if (n === 1) {
-    return true;
-  } else {
-    //Loop to continually divide n by 2.
-    while (n > 1) {
-      if (n % 2 != 0) {
-        return false;
-      }
-      n = n / 2;
-    }
-  }
-  //If an even number is divided by 2 to eventually equal 1, it is a power of 2.
-  return n === 1;
+  //.. should return true or false ..
+  return Number.isInteger(Math.log2(n));
 }
